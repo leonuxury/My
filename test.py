b5b9f6f1d8f28ed7fbc4a6d156027654c61305d3
li_jing_ok <li_jing_ok@aliyun.com>
2020-06-05 17:53:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,16 +5,14 @@ SMTP_HOST = 'smtp.gmail.com'
 USER = 'leonuxury@gmail.com'
 PASSWORD = 'LJlj11!!'
 TO = '1005411336@qq.com'
-smtp = SMTP_SSL(host=SMTP_HOST)
-smtp.starttls()
-smtp.set_debuglevel(True)
-smtp.login(user=USER, password=PASSWORD)
+with SMTP_SSL(host=SMTP_HOST) as smtp:
+    smtp.login(user=USER, password=PASSWORD)
 
-msg = MIMEText(f'请关注：，当前处于盈利状态。', _charset="utf8")
-msg["Subject"] = f'请关注，当前处于盈利状态。'
-msg['From'] = USER  # 发送者
-msg['To'] = TO  # 接收者
+    msg = MIMEText(f'请关注：，当前处于盈利状态。', _charset="utf8")
+    msg["Subject"] = f'请关注 ，当前处于盈利状态。'
+    msg['From'] = USER  # 发送者
+    msg['To'] = TO  # 接收者
 
-smtp.sendmail(from_addr=USER,
-              to_addrs=[TO],
-              msg=msg.as_string())
+    smtp.sendmail(from_addr=USER,
+                  to_addrs=[TO],
+                  msg=msg.as_string())
